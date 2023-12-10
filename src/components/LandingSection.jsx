2dff198aebac3e9df5a6e0cef684d74f9f774025
Anant Singh <38243636+anantSinghCross@users.noreply.github.com>
2023-12-10 20:51:06 +0530

    Background image refactor

--- src/components/LandingSection.jsx
+++ src/components/LandingSection.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import bgImage from '../../src/assets/Vector.png';
 
 export default function LandingSection(){
+    console.log(bgImage)
+    // using styles just for the background image
+    const styles = {
+        backgroundImage: `url('${bgImage}')`,
+    }
     return (
-        <div className='landing'>
+        <div className='landing' style={styles}>
             <div>
                 <h2>Hi, I'm Anant</h2>
                 <hr className='landing-hr-line'/>
