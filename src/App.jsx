1b731a45e395ea909395de19d4d43c66b8cd8014
Anant Singh <38243636+anantSinghCross@users.noreply.github.com>
2024-05-13 21:40:20 +0530

    Rebuilding from scratch. Removed all components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,8 @@
-import { useResumeCollection } from './hooks/useResumeCollection';
-import SimpleSection from './components/SimpleSection'
-import WorkExSection from './components/WorkExSection'
-import ProjectSection from './components/ProjectSection'
-import LandingSection from './components/LandingSection';
-
 function App() {
-  const resume = useResumeCollection(); // 'resume' array will contain some objects whose 'content' key does not contain a string 
-  const resumeList = resume.length > 0 ? 
-    resume
-    .sort((a, b) => a.sequence - b.sequence)
-    .map((doc) => {
-      if(!Array.isArray(doc.content)){
-        return <SimpleSection key={doc.id} title={doc.title} content={doc.content} />
-      } else if (doc.type === 'workEx') {
-        return <WorkExSection key={doc.id} title={doc.title} content={doc.content} />
-      } else if (doc.type === 'projects') {
-        return <ProjectSection key={doc.id} title={doc.title} content={doc.content} />
-      }
-    }) : 'Loading...';
-
-  // console.log(resume)
   return (
     <>
-      <LandingSection/>
-      {resumeList}
+      Anant Singh
+      Frontend React Developer
     </>
   )
 }
--- src/components/Crumb.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Crumb({content}){
-    return (
-        <div>{content}</div>
-    )
-}
