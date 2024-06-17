725f31770fda92c55cba777882c7ae9c62fdc924
Anant Singh <38243636+anantSinghCross@users.noreply.github.com>
2024-06-18 01:14:20 +0530

    Mobile responsiveness

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -25,37 +25,37 @@ function Landing() {
         </div>
         <div className="flex flex-row sm:flex-col gap-4 flex-wrap py-4 sm:py-0">
           <a
-            className=" p-2 px-4 sm:p-3 sm:px-8 rounded-full bg-gradient-to-tr from-blue-800 to-blue-500 shadow-xl shadow-blue-950 text-zinc-200 text-center hover:scale-105 transition-all"
+            className=" p-2 px-4 sm:p-3 sm:px-8 rounded-full bg-gradient-to-tr from-blue-800 to-blue-500 shadow-lg shadow-blue-950 text-zinc-200 text-center hover:scale-105 hover:shadow-blue-900 transition-all"
             href="https://www.linkedin.com/in/anantsinghcross/"
           >
-            <span className="flex items-center gap-5">
+            <span className="flex items-center gap-2 sm:gap-5">
               <FaLinkedinIn size={20} />
               <p>LinkedIn</p>
             </span>
           </a>
           <a
-            className=" p-2 px-4 sm:p-3 sm:px-8 rounded-full bg-gradient-to-tr from-pink-800 to-pink-500 shadow-xl shadow-pink-950 text-zinc-200 text-center hover:scale-105 transition-all"
+            className=" p-2 px-4 sm:p-3 sm:px-8 rounded-full bg-gradient-to-tr from-pink-800 to-pink-500 shadow-lg shadow-pink-950 text-zinc-200 text-center hover:scale-105 hover:shadow-pink-900 transition-all"
             href="https://www.instagram.com/oregano.seasoning/"
           >
-            <span className="flex items-center gap-5">
+            <span className="flex items-center gap-2 sm:gap-5">
               <BsInstagram size={20} />
               <p>Instagram</p>
             </span>
           </a>
           <a
-            className=" p-2 px-4 sm:p-3 sm:px-8 rounded-full bg-gradient-to-tr from-green-800 to-green-500 shadow-xl shadow-green-950 text-zinc-200 text-center hover:scale-105 transition-all"
+            className=" p-2 px-4 sm:p-3 sm:px-8 rounded-full bg-gradient-to-tr from-green-800 to-green-500 shadow-lg shadow-green-950 text-zinc-200 text-center hover:scale-105 hover:shadow-green-900 transition-all"
             href="https://github.com/anantSinghCross"
           >
-            <span className="flex items-center gap-5">
+            <span className="flex items-center gap-2 sm:gap-5">
               <FiGithub size={20} />
               <p>GitHub</p>
             </span>
           </a>
           <a
-            className=" p-2 px-4 sm:p-3 sm:px-8 rounded-full bg-gradient-to-tr from-neutral-800 to-neutral-500 shadow-xl shadow-neutral-950 text-zinc-200 text-center hover:scale-105 transition-all"
+            className=" p-2 px-4 sm:p-3 sm:px-8 rounded-full bg-gradient-to-tr from-neutral-800 to-neutral-500 shadow-lg shadow-neutral-950 text-zinc-200 text-center hover:scale-105 hover:shadow-neutral-900 transition-all"
             href="mailto:anantsingh.cse@gmail.com"
           >
-            <span className="flex items-center gap-5">
+            <span className="flex items-center gap-2 sm:gap-5">
               <TbMailbox size={20} />
               <p>Email</p>
             </span>
